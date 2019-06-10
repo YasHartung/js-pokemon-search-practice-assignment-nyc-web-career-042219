925a2a3878de281a14ffcabe894b5ef7e5f8c10c
Yasmine <y.hartung@gmail.com>
2019-06-10 15:33:13 -0400

    added some crud actions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+
 document.addEventListener('DOMContentLoaded', () => {
   // console.log(POKEMON)
   //YOUR CODE HERE
@@ -6,12 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let pokemonContainer = document.querySelector('#pokemon-container')
   let searchBar = document.querySelector('#pokemon-search-input')
+  let newPokemonForm = document.querySelector('#new-pokemon-form')
+  
 
   function displayPokemon(data) {
     pokemonContainer.innerHTML = data.map(function(pokemon) {
       return `
         <div class="pokemon-card">
         <div class="pokemon-frame">
+          <button class="delete-button" data-id="${pokemon.id}">X</button>
           <h1 class="center-text">${pokemon.name}</h1>
           <div class="pokemon-image">
             <img data-id="${pokemon.id}" data-action="flip" class="toggle-sprite" src="${pokemon.sprites.front}">
@@ -23,30 +28,46 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   pokemonContainer.addEventListener('click', event => {
-    console.log(event.target.dataset.id)
-    if (!!event.target.dataset.id) {
-      console.log(event.target.src)
+    if (event.target.className === "toggle-sprite") {
+    
       let target = pokemonArray.find(pokemon => pokemon.id == event.target.dataset.id)
-      console.log(target)
+    
       if (event.target.src === target.sprites.front)
-       {
+      {
         event.target.src = target.sprites.back
       }
       else {
         event.target.src = target.sprites.front
       }
     }
+    if (event.target.className === "delete-button"){
+
+      deletePokemon(event.target.dataset.id);
+    }
+    
   })
 
- searchBar.addEventListener('keydown', event => {
-    console.log(event)
-    console.log(searchBar.value)
+ searchBar.addEventListener('input', event => {
+   
     filterPokemon(pokemonArray, searchBar.value)
   })
+  newPokemonForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    pokeName = e.target[0].value
+    pokeFront = e.target[1].value
+    pokeBack = e.target[2].value
+
+    createPokemon(pokeName, pokeFront, pokeBack)
+
+  })
+
+
+
   
   function filterPokemon(pokemonArray, query) {
-    let actuallyFilter = pokemonArray.filter(function(el) {
-      return el.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    let actuallyFilter = pokemonArray.filter(pokemon => {
+      return pokemon.name.toLowerCase().includes(query)
     })
     displayPokemon(actuallyFilter)
   }
@@ -56,9 +77,60 @@ document.addEventListener('DOMContentLoaded', () => {
     return response.json();
   })
   .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
+    
     pokemonArray = myJson
     displayPokemon(pokemonArray)
   });
+
+  function createPokemon(name, front, back){
+    fetch('http://localhost:3000/pokemon', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+     
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+  
+      body: JSON.stringify({name: name, sprites:{front: front, back: back}}),
+         // body data type must match "Content-Type" header
+  })
+  .then(response => response.json())
+  .then(data => {
+    
+   
+    pokemonArray.push(data)
+    // debugger
+    
+    displayPokemon(pokemonArray);
+  }); // parses JSON response into native Javascript objects 
+
+}
+
+function deletePokemon(id){
+
+  
+    return fetch('http://localhost:3000/pokemon' + '/' + id, {
+      method: 'DELETE'
+    })
+    .then(response => {
+      response.json()
+      
+      let newPokeArr = pokemonArray.filter(function(pokemon){
+        return pokemon.id != id
+      })
+
+      pokemonArray = newPokeArr;
+      
+      displayPokemon(pokemonArray);
+      
+    })
+    
+    
+  
+}
+
+  
+
 
 })
